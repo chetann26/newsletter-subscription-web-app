c48de977676cfb96160132f24236ebcacfa6eef5
chetann26 <chetanmpatidar100@gmail.com>
2022-05-26 15:13:45 +0530

    added server files

---
+++ app.js
@@ -0,0 +1,57 @@
+const express=require('express');
+//const  request=require('request');
+const bodyParser=require('body-parser');
+const { stringify } = require('querystring');
+const https=require('https');
+const app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
+app.post("/",function(req,res){
+    var firstname=req.body.fname;
+    var lastname=req.body.lname;
+    var email=req.body.email;
+    
+    const data={
+        members:[
+            {
+                email_address: email,
+                status : "subscribed",
+                merge_fields: {
+                    FNAME: firstname,
+                    LNAME: lastname
+                }
+            } 
+        ]
+    };
+    const jsonData=JSON.stringify(data);
+    const url="https://us17.api.mailchimp.com/3.0/lists/e6280019ca "
+    const options= {
+        method:"POST",
+        auth: "cadet26:e9a34aaf3287d4906413c9be6539a9e0-us17"
+    }
+    const request=https.request(url,options,function(response){
+          response.on("data",function(data){
+              console.log(JSON.parse(data));
+          })
+          var filename;
+          if(response.statusCode===200){
+              filename="/success.html";
+          }else{
+              filename="/fail.html";
+          }
+          res.sendFile(__dirname+filename);
+     }); 
+    request.write(jsonData);
+    request.end(); 
+    
+});
+app.listen(process.env.PORT || 3000,function(){
+    console.log("server listening at port 3000");
+});
+
+// api key : e9a34aaf3287d4906413c9be6539a9e0-us17
+//list id: e6280019ca. or 1356092
